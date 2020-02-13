3f08ad5377a1230f55e8f7f265f76a702d7001f9
Beza Sirak <sirak.bezawit@gmail.com>
2020-02-13 01:26:16 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,17 +44,23 @@ def naming_system
   naming_system ={ 
     label: "Kingdom",
     sub_category: {
-      label: "phylem"
+      label: "phylem",
       sub_category: {
-        label: "Class"
+        label: "Class",
         sub_category: {
-          label: "Order"
-          sub_category
+          label: "Order",
+          sub_category: {
+            label: "Family",
+            sub_category: {
+              label: "Genus",
+              sub_category :{
+                label: "Species"
+              }
+            }
+          }
         }
       }
     }
-    
-    
   }
 
 end
